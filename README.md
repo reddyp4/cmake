a3c4ff314573772591864f5ee5fcf50741db2ed9
Patel-Windows <reddynisky145@gmail.com>
2024-12-27 11:58:15 -0800

    basic cmake

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cmake
 cmake build system for cpp
+
+Why CMake?
+Can handle dependencies
+Supports cross-platform development
+
+.json:
+Group settings together using .json
+CMakePresets.json: multiple presets, add to continuous integration system, share configs between developers
+
+Source: cmake.org
+
+Step1: Building Basic Project
+Minimum commands: 
+cmake_minimum_required(): 
+project()
+add_executable()
---
+++ main.cpp
@@ -0,0 +1,22 @@
+// A simple program 
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    /*if(argc < 2)
+    {
+        return 1;
+    }*/
+    // convert input to double
+    //const double inputValue = atof(argv[1]);
+
+    //calculate square root
+    //const double outputValue = sqrt(inputValue);
+    //std::cout << "Square root of " << inputValue << "is" << outputValue << std::endl;
+    
+    std::cout << "Entering main " << std::endl;
+    return 0;
+}
