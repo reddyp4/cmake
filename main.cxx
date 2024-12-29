f0d95b71a04eb562f206aed1ad9a5f9fa4348753
Patel-Windows <reddynisky145@gmail.com>
2024-12-28 17:57:54 -0800

    Added the sqrt internal function and choice

--- main.cxx
+++ main.cxx
@@ -1,9 +1,9 @@
 // A simple program 
 #include <cmath>
-#include <cstdlib>
 #include <iostream>
 #include <string>
 
+#include "MathFunctions.h"
 #include "Config.h"
 
 int main(int argc, char* argv[])
@@ -11,18 +11,19 @@ int main(int argc, char* argv[])
     if(argc < 2)
     {
         //Report version
-        /*std::cout << argv[0] << " Version " << Main_VERSION_MAJOR << "."
+        /*
+        std::cout << argv[0] << " Version " << Main_VERSION_MAJOR << "."
                   << Main_VERSION_MINOR << std::endl;
-        std::cout << "Usage " << argv[0] << "number" << std::endl;*/
+        std::cout << "Usage: " << argv[0] << "number" << std::endl;*/
         return 1;
     }
     // convert input to double
     const double inputValue = atof(argv[1]);
 
-    //calculate square root
-    const double outputValue = sqrt(inputValue);
+    //calculate square root with internal function
+    const double outputValue = mathfunctions::sqrt(inputValue);
     std::cout << "Square root of " << inputValue << " is " << outputValue << std::endl;
     
-    std::cout << "Entering main " << std::endl;
+    std::cout << "Finished main " << std::endl;
     return 0;
 }
